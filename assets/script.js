86e4f8bf553d10cc97e0f17b6432cc9fed576a50
devitodev <softwaredev.ito@gmail.com>
2022-11-19 09:10:15 -0600

    added jquery check

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,15 @@
-var penguinImage = $('.penguin-container').children();
+var penguinImage = document.querySelector('.penguin-pic')
 
 
 
+console.log(penguinImage)   
 
-    
+$(document).ready(function(){
+    if (jQuery) {  
+      // jQuery is loaded  
+      alert("Yeah!");
+    } else {
+      // jQuery is not loaded
+      alert("Doesn't Work");
+    }
+  });
