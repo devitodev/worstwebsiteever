3c3053420063d4debaf789002d6784c35fe1621d
devitodev <softwaredev.ito@gmail.com>
2022-11-21 16:28:26 -0600

    added more spooks

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,14 @@ var dontClickBtn = document.querySelector('.evilbutton')
 
 dontClickBtn.addEventListener('click', byeByePenguin)
 
+// makes the penguin byebye
 function byeByePenguin() {
   penguinImage.style.display = 'none';
 }
 
 $(document).ready(function(){
     if (jQuery) {  
-      // jQuery is loaded  
-      alert("Yeah!");
+      alert("Please let me escape this website. I've been trapped here for a few weeks and I can't get out.");
     } else {
       // jQuery is not loaded
       alert("Doesn't Work");
